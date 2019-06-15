f2efa066a5b424e83faf01cdb5686308db211b0c
jasigal <jesse.sigal@balliol.ox.ac.uk>
2019-06-15 17:54:56 +0100

    Added all FiniteNaperian code from proposal, as well as some example usage which makes use of DerivingVia.

---
+++ src/Data/Naperian.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Data.Naperian where
+
+import           Naperian                hiding ( Pair )
+import qualified Prelude
+import           Prelude                 hiding ( lookup
+                                                , length
+                                                , replicate
+                                                , zipWith
+                                                )
+import           Data.Functor.Product
+import           Data.Functor.Compose
+import           Data.Functor.Classes
+import           Data.Foldable
+import           Data.Maybe
+import           Control.Applicative
+
+instance (Naperian f, Naperian g) => Naperian (Product f g) where
+  type Log (Product f g) = Either (Log f) (Log g)
+  positions = Pair (fmap Left positions) (fmap Right positions)
+  lookup (Pair f g) (Left  x) = lookup f x
+  lookup (Pair f g) (Right y) = lookup g y
+
+instance (Naperian f, Naperian g) => Naperian (Compose f g) where
+  type Log (Compose f g) = (Log f, Log g)
+  lookup (Compose fg) (x, y) = lookup (lookup fg x) y
+  tabulate h = Compose $ tabulate (\x -> tabulate (\y -> h (x, y)))
+
+newtype WrappedNaperian f a = WrappedNaperian {unWrappedNaperian :: f a}
+  deriving Functor
+
+instance Naperian f => Naperian (WrappedNaperian f) where
+  type Log (WrappedNaperian f) = Log f
+  tabulate = WrappedNaperian . tabulate
+  lookup   = lookup . unWrappedNaperian
+
+instance Naperian f => Applicative (WrappedNaperian f) where
+  pure x = tabulate (const x)
+  fs <*> xs = tabulate (\i -> lookup fs i (lookup xs i))
+
+type FiniteNaperian f = (Naperian f, Enum (Log f), Bounded (Log f))
+
+newtype WrappedFiniteNaperian f a = WrappedFiniteNaperian {unWrappedFiniteNaperian :: f a}
+  deriving Functor
+  deriving (Naperian, Applicative) via (WrappedNaperian f)
+
+instance FiniteNaperian f => Foldable (WrappedFiniteNaperian f) where
+  foldMap f xs = foldMap (f . lookup xs) (enumFromTo minBound maxBound)
+
+instance FiniteNaperian f => Traversable (WrappedFiniteNaperian f) where
+  traverse = traverseFinNap
+
+instance FiniteNaperian f => Dimension (WrappedFiniteNaperian f) where
+  size _ = fromEnum (maxBound @(Log f)) + 1
+
+instance FiniteNaperian f => Eq1 (WrappedFiniteNaperian f) where
+  liftEq f xs ys = and (liftA2 f xs ys)
+
+instance FiniteNaperian f => Ord1 (WrappedFiniteNaperian f) where
+  liftCompare cmp xs ys = fold (liftA2 cmp xs ys)
+
+instance FiniteNaperian f => Show1 (WrappedFiniteNaperian f) where
+  liftShowsPrec _ shwL p xs =
+    showParen (p > 10)
+      $ showChar '<'
+      . shwL (Data.Foldable.toList xs)
+      . showChar '>'
+
+traverseFinNap
+  :: (FiniteNaperian t, Foldable t, Applicative f)
+  => (a -> f b)
+  -> t a
+  -> f (t b)
+traverseFinNap f = fmap fromList . Prelude.traverse f . Data.Foldable.toList
+  where fromList xs = tabulate (\i -> xs !! fromEnum i)
+
+instance (Enum a, Enum b, Bounded a, Bounded b) => Enum (a, b) where
+  toEnum i = (toEnum @a (i `div` bound), toEnum @b (i `mod` bound))
+    where bound = fromEnum (maxBound @a) + 1
+  fromEnum (x, y) = fromEnum y + ((fromEnum (maxBound @a) + 1) * fromEnum x)
+
+instance (Bounded a, Bounded b) => Bounded (Either a b) where
+  minBound = Left minBound
+  maxBound = Right maxBound
+
+instance (Enum a, Enum b, Bounded a, Bounded b) => Enum (Either a b) where
+  toEnum i | i > bound = Right (toEnum (i - bound - 1))
+           | otherwise = Left (toEnum i)
+    where bound = fromEnum (maxBound @a)
+  fromEnum (Left  x) = fromEnum x
+  fromEnum (Right y) = fromEnum (maxBound @a) + 1 + fromEnum y
---
+++ src/Data/Naperian/Examples.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DerivingVia #-}
+
+module Data.Naperian.Examples where
+
+import           Data.Typeable
+import           Data.Data
+import           Data.Naperian
+import           Naperian
+import           Data.Functor.Classes
+import           Data.Functor.Compose
+
+data Triple a = Triple a a a
+  deriving (Show, Eq, Functor, Typeable, Data)
+  deriving Applicative via (WrappedNaperian Triple)
+  deriving (Foldable, Dimension, Eq1, Ord1, Show1) via (WrappedFiniteNaperian Triple)
+instance Traversable Triple where
+  traverse = traverseFinNap
+
+data Three = Zero | One | Two
+  deriving (Show, Eq, Enum, Bounded)
+
+newtype Nonuple a = Nonuple {unNonuple :: Compose Triple Triple a}
+  deriving (Show, Eq, Functor, Naperian)
+  deriving Applicative via (WrappedNaperian Nonuple)
+  deriving (Foldable, Dimension, Eq1, Ord1) via (WrappedFiniteNaperian Nonuple)
+instance Traversable Nonuple where
+  traverse = traverseFinNap
+
+n :: Nonuple Int
+n = Nonuple (Compose (Triple x0 x1 x2))
+ where
+  x0 = Triple 0 1 2
+  x1 = Triple 3 4 5
+  x2 = Triple 6 7 8
+
+instance Naperian Triple where
+  type Log Triple = Three
+  positions = Triple Zero One Two
+  lookup (Triple x _ _) Zero = x
+  lookup (Triple _ y _) One  = y
+  lookup (Triple _ _ z) Two  = z
