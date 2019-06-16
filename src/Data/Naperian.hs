db88b5f16307837ceed8a6bef0bc5f8324215660
jasigal <jesse.sigal@balliol.ox.ac.uk>
2019-06-16 12:20:33 +0100

    Added documentation.

--- src/Data/Naperian.hs
+++ src/Data/Naperian.hs
@@ -29,61 +29,85 @@ import           Control.Applicative
 import           GHC.TypeLits
 import           GHC.Exts
 
+-- | The 'Identity' functor is Naperian with log @()@.
 instance Naperian Identity where
   type Log Identity = ()
   positions = Identity ()
   lookup (Identity x) () = x
 
+-- | When @f@ and @g@ are Naperian functors, then so is @'Product' f g@ with
+-- log @('Log' f) + ('Log' g)@.
 instance (Naperian f, Naperian g) => Naperian (Product f g) where
   type Log (Product f g) = Either (Log f) (Log g)
   positions = Pair (fmap Left positions) (fmap Right positions)
   lookup (Pair f g) (Left  x) = lookup f x
   lookup (Pair f g) (Right y) = lookup g y
 
+-- | A 'Product' of finite Naperian functors is automatically a 'Dimension'.
 deriving via (WrappedFiniteNaperian (Product f g)) instance
   (FiniteNaperian f, FiniteNaperian g, Dimension f, Dimension g) => Dimension (Product f g)
 
+-- | When @f@ and @g@ are Naperian functors, then so is @'Compose' f g@ with
+-- log @('Log' f) * ('Log' g)@.
 instance (Naperian f, Naperian g) => Naperian (Compose f g) where
   type Log (Compose f g) = (Log f, Log g)
   lookup (Compose fg) (x, y) = lookup (lookup fg x) y
   tabulate h = Compose $ tabulate (\x -> tabulate (\y -> h (x, y)))
 
+-- | A 'Product' of finite Naperian functors is automatically a 'Dimension'.
 deriving via (WrappedFiniteNaperian (Compose f g)) instance
   (FiniteNaperian f, FiniteNaperian g, Dimension f, Dimension g) => Dimension (Compose f g)
 
+-- | A newtype wrapper around a Naperian functor for automatically deriving
+-- instances using 'DerivingVia'.
 newtype WrappedNaperian f a = WrappedNaperian {unWrappedNaperian :: f a}
   deriving Functor
 
+-- | Lifts underlying 'Naperian' instance.
 instance Naperian f => Naperian (WrappedNaperian f) where
   type Log (WrappedNaperian f) = Log f
   tabulate = WrappedNaperian . tabulate
   lookup   = lookup . unWrappedNaperian
 
+-- | All Naperian functors are 'Applicative' in a pointwise manner.
 instance Naperian f => Applicative (WrappedNaperian f) where
   pure x = tabulate (const x)
   fs <*> xs = tabulate (\i -> lookup fs i (lookup xs i))
 
+-- | Constraints for the class of Naperian functors which are represented by
+-- a finite type.
 type FiniteNaperian f = (Naperian f, Enum (Log f), Bounded (Log f))
 
+-- | A newtype wrapper around a finite Naperian functor for automatically
+-- deriving instances using 'DerivingVia'.
 newtype WrappedFiniteNaperian f a = WrappedFiniteNaperian {unWrappedFiniteNaperian :: f a}
   deriving Functor
   deriving (Naperian, Applicative) via (WrappedNaperian f)
 
+-- | All finite Naperian functors can be giving a 'Foldable' instance by using
+-- the list instance.
 instance FiniteNaperian f => Foldable (WrappedFiniteNaperian f) where
   foldMap f xs = foldMap (f . lookup xs) (enumFromTo minBound maxBound)
 
+-- | All finite Naperian functors can be giving a 'Traversable' instance by
+-- using the list instance.
 instance FiniteNaperian f => Traversable (WrappedFiniteNaperian f) where
   traverse = traverseFinNap
 
+-- | All finite Naperian functors trivially have a 'Dimension' instance.
 instance FiniteNaperian f => Dimension (WrappedFiniteNaperian f) where
   size _ = fromEnum (maxBound @(Log f)) + 1
 
+-- | All finite Naperian functors have 'Eq1' instances by working pointwise.
 instance FiniteNaperian f => Eq1 (WrappedFiniteNaperian f) where
   liftEq f xs ys = and (liftA2 f xs ys)
 
+-- | All finite Naperian functors have an 'Ord1' instance by interpreting them
+-- as n-ary homogeneous tuples.
 instance FiniteNaperian f => Ord1 (WrappedFiniteNaperian f) where
   liftCompare cmp xs ys = fold (liftA2 cmp xs ys)
 
+-- | A useful instance to allow deriving of 'Show' for 'Product' and 'Compose'.
 instance FiniteNaperian f => Show1 (WrappedFiniteNaperian f) where
   liftShowsPrec _ shwL p xs =
     showParen (p > 10)
@@ -91,6 +115,13 @@ instance FiniteNaperian f => Show1 (WrappedFiniteNaperian f) where
       . shwL (Data.Foldable.toList xs)
       . showChar '>'
 
+-- | We cannot use 'DerivingVia' to derive a 'Traversable' instance for finite
+-- Naperian functors. Essentially, 'DerivingVia' works using the 'Coercible'
+-- type class and its member @coerce@. Tyring to deriving 'Traversable' will
+-- attempt to use @coerce@ under the 'Applicative' functor 'f'. However, this
+-- is only valid if 'f's argument has a nominal role, which is not guarunteed
+-- in the signature. For a more detailed explanation, see
+-- <https://ryanglscott.github.io/2018/06/22/quantifiedconstraints-and-the-trouble-with-traversable/ here>.
 traverseFinNap
   :: (FiniteNaperian t, Foldable t, Applicative f)
   => (a -> f b)
@@ -99,15 +130,18 @@ traverseFinNap
 traverseFinNap f = fmap fromList . Prelude.traverse f . Data.Foldable.toList
   where fromList xs = tabulate (\i -> xs !! fromEnum i)
 
+-- | A lexicographic enumeration for pairs.
 instance (Enum a, Enum b, Bounded a, Bounded b) => Enum (a, b) where
   toEnum i = (toEnum @a (i `div` bound), toEnum @b (i `mod` bound))
     where bound = fromEnum (maxBound @a) + 1
   fromEnum (x, y) = fromEnum y + ((fromEnum (maxBound @a) + 1) * fromEnum x)
 
+-- | A bound for sums where the left is ordered below the right.
 instance (Bounded a, Bounded b) => Bounded (Either a b) where
   minBound = Left minBound
   maxBound = Right maxBound
 
+-- | A left-biased enumeration of a sum.
 instance (Enum a, Enum b, Bounded a, Bounded b) => Enum (Either a b) where
   toEnum i | i > bound = Right (toEnum (i - bound - 1))
            | otherwise = Left (toEnum i)
--- src/Torch/Naperian/Examples.hs
+++ src/Torch/Naperian/Examples.hs
@@ -34,6 +34,8 @@ network l1 l2 = layer l2 . layer l1
 
 type I = Identity
 
+-- | Given @f@-shaped input @x@, we predict scalar output @y@ by storing an
+-- @f@-shape of slopes @m@ and a bias @c@ for the model @y = (m . x) + c@.
 newtype LinearModel f a = LinearModel {unLinearModel :: Product f I a}
   deriving (Show, Eq, Functor, Naperian)
 deriving via (WrappedNaperian (LinearModel f)) instance
@@ -45,6 +47,7 @@ instance (FiniteNaperian f) => Traversable (LinearModel f) where
 deriving via (WrappedFiniteNaperian (LinearModel f)) instance
   (FiniteNaperian f) => Dimension (LinearModel f)
 
+-- | A sample of an @f@-shape input and a scalar output.
 newtype Sample f a = Sample {unSample :: Product f I a}
   deriving (Show, Eq, Functor, Naperian)
 deriving via (WrappedNaperian (Sample f)) instance
@@ -60,6 +63,7 @@ instance (FiniteNaperian f) => Traversable (Sample f) where
 deriving via (WrappedFiniteNaperian (Sample f)) instance
   (FiniteNaperian f) => Dimension (Sample f)
 
+-- | A @g@-shape amount of @f@-shape samples.
 newtype Samples g f a = Samples {unSamples :: Compose g (Sample f) a}
   deriving (Functor, Naperian)
 deriving instance (FiniteNaperian f, Show1 g) => Show1 (Samples g f)
@@ -75,6 +79,7 @@ deriving via (WrappedFiniteNaperian (Samples g f)) instance
 instance (FiniteNaperian g, FiniteNaperian f) => Traversable (Samples g f) where
   traverse = traverseFinNap
 
+-- | Given samples and a model, calculate the MSE.
 totalError
   :: forall a f g
    . (Num a, Dimension f, Dimension g)
@@ -90,6 +95,7 @@ totalError (Samples (Compose samples)) (LinearModel model) = sum
     let Pair input (Identity output) = sample
     in  (output - (inner input slopes + intercept)) ^ 2
 
+-- | Use gradient descent on MSE to produce a stream of more accurate models.
 fitModel
   :: (Fractional a, Ord a, Dimension f, FiniteNaperian f, Dimension g)
   => Samples g f a
@@ -97,11 +103,14 @@ fitModel
   -> [LinearModel f a]
 fitModel samples start = gradientDescent (totalError (fmap auto samples)) start
 
+-- | Example samples, essentially @[([0], 0), ([1], 1), ..., ([9], 9)]@ for
+-- @(inputs, output)@.
 samples :: Samples (Vector 10) (Vector 1) Double
 samples = Samples . Compose $ fmap f viota
  where
   f :: Finite 10 -> Sample (Vector 1) Double
   f (Fin i) = let v = fromIntegral i :: Double in pure v
 
+-- | Use 'fitModel' for 100 steps starting with the model @m = 0@ and @c = 0@.
 fittedModel :: LinearModel (Vector 1) Double
 fittedModel = fitModel samples (pure 0) !! 100
