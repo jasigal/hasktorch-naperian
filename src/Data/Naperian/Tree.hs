020c16dbe370bf2c3d048620e83e6e291b5071d8
jasigal <jesse.sigal@balliol.ox.ac.uk>
2019-07-15 12:26:08 +0200

    WIP mapAccum, CCC

--- src/Data/Naperian/Tree.hs
+++ src/Data/Naperian/Tree.hs
@@ -12,6 +12,10 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Data.Naperian.Tree where
 
@@ -21,6 +25,7 @@ import           Data.Singletons
 import           Data.Singletons.TH
 import           Data.Singletons.TypeLits
 import           Data.Singletons.Prelude.Num
+import           Data.Kind
 
 $(singletons [d|
   data TShape where
@@ -114,3 +119,154 @@ instance SingI s => Traversable (Tree s) where
     STOp sl sr -> case t of
       TBranch l r ->
         TBranch <$> withSingI sl (traverse f l) <*> withSingI sr (traverse f r)
+
+data TreeS a (f :: TShape -> Type) (s :: TShape) where
+  TLeafS :: a -> TreeS a f TUnit
+  TBranchS :: a -> f l -> f r -> TreeS a f (TOp l r)
+
+newtype SFix f s = SFix {unSFix :: f (SFix f) s}
+
+-- Indexed types form a cartesian closedcategory
+
+-- Homset
+type f :-> g = forall s. f s -> g s
+-- Exponential
+newtype (:=>) f g s = Exp {unExp :: f s -> g s}
+
+-- Internalize morphism
+sinternal :: (f :-> g) -> (SConst () :-> (f :=> g))
+sinternal func _ = Exp func
+
+-- Externalize morphism
+sexternal :: (SConst () :-> (f :=> g)) -> (f :-> g)
+sexternal int = let Exp func = int (SConst ()) in func
+
+-- Eval
+seval :: ((f :=> g) :*: f) :-> g
+seval (P func val) = unExp func val
+
+-- Internal curry
+sicurry :: ((f :*: g) :=> e) :-> (f :=> (g :=> e))
+sicurry (Exp func) = Exp $ \f -> Exp $ \g -> func (P f g)
+
+-- Internal uncurry
+siuncurry :: (f :=> (g :=> e)) :-> ((f :*: g) :=> e)
+siuncurry (Exp fge) = Exp $ \(P f g) -> let Exp ge = fge f in ge g
+
+-- External curry
+securry :: ((f :*: g) :-> e) -> (f :-> (g :=> e))
+securry func f = Exp $ \g -> func (P f g)
+
+-- External uncurry
+seuncurry :: (f :-> (g :=> e)) -> ((f :*: g) :-> e)
+seuncurry fge (P f g) = let Exp ge = fge f in ge g
+
+class ShapeFunctor (h :: (TShape -> Type) -> (TShape -> Type)) where
+  sfmap :: (f :-> g) -> (h f :-> h g)
+
+class ShapeFunctor h => ShapeApply h where
+  sdist :: (h f :*: h g) :-> h (f :*: g)
+  -- sap :: h (f :=> g) :-> (h f :=> h g)
+  -- sap = securry (sfmap seval . sdist)
+
+type h :~> k = forall s f. h f s -> k f s
+
+class (ShapeFunctor h, ShapeFunctor h') => Patchable h h' where
+  patch :: forall s f g. h f s -> h' g s -> h' f s
+
+instance ShapeFunctor (TreeS a) where
+  sfmap f (TLeafS a) = TLeafS a
+  sfmap f (TBranchS a t1 t2) = TBranchS a (f t1) (f t2)
+
+instance Monoid a => ShapeApply (TreeS a) where
+  sdist (P (TLeafS af) (TLeafS ag)) = TLeafS (af <> ag)
+  sdist (P (TBranchS af t1f t2f) (TBranchS ag t1g t2g)) =
+    TBranchS (af <> ag) (P t1f t1g) (P t2f t2g)
+
+-- Replace f by g in left h
+replace :: ShapeApply h => (h f :*: h g) :-> h g
+replace = sfmap proj2 . sdist
+
+instance Patchable (TreeS a) (TreeS b) where
+  patch (TLeafS _) (TLeafS a) = (TLeafS a)
+  patch (TBranchS _ f1 f2) (TBranchS b _ _) = TBranchS b f1 f2
+
+scata
+  :: ShapeFunctor h
+  => (h f :-> f)
+  -> (SFix h :-> f)
+scata alg = alg . sfmap (scata alg) . unSFix
+
+newtype SConst a s = SConst {unSConst :: a}
+instance Show a => Show (SConst a s) where
+  show (SConst a) = "SConst " ++ show a
+
+data (f :*: g) s = P (f s) (g s)
+  deriving Show
+
+proj1 :: (f :*: g) :-> f
+proj1 (P f _) = f
+
+proj2 :: (f :*: g) :-> g
+proj2 (P _ g) = g
+
+shapeDist :: ShapeFunctor h => h (f :*: g) :-> (h f :*: h g)
+shapeDist x = P (sfmap proj1 x) (sfmap proj2 x)
+
+treeSpec
+  :: (a -> (b, c))
+  -> (a -> b -> b -> (b, c))
+  -> (TreeS a (SConst b)) :-> (SConst b :*: TreeS c (SConst ()))
+treeSpec f g (TLeafS a) = let (b, c) = f a in P (SConst b) (TLeafS c)
+treeSpec f g (TBranchS a (SConst b1) (SConst b2)) =
+  let (b, c) = g a b1 b2 in P (SConst b) (TBranchS c (SConst ()) (SConst ()))
+
+treeAlg
+  :: (a -> (b, c))
+  -> (a -> b -> b -> (b, c))
+  -> (TreeS a (SConst b :*: SFix (TreeS c)) :-> (SConst b :*: SFix (TreeS c)))
+treeAlg f g (TLeafS a) = let (b, c) = f a in P (SConst b) (SFix $ TLeafS c)
+treeAlg f g (TBranchS a p1 p2) =
+  let P (SConst b1) t1 = p1
+      P (SConst b2) t2 = p2
+      (b, c) = g a b1 b2
+  in P (SConst b) (SFix $ TBranchS c t1 t2)
+
+treeSpecToAlg
+  :: TreeS a (SConst b) :-> (SConst b :*: TreeS c (SConst ()))
+  -> TreeS a (SConst b :*: SFix (TreeS c)) :-> (SConst b :*: SFix (TreeS c))
+treeSpecToAlg spec level =
+  case shapeDist level of
+    P bs cs ->
+      let P b c = spec bs
+      in P b (SFix $ patch cs c)
+
+sMapAccumTree
+  :: (TreeS a (SConst b :*: SFix (TreeS c)):-> (SConst b :*: SFix (TreeS c)))
+  -> SFix (TreeS a) s
+  -> (b, SFix (TreeS c) s)
+sMapAccumTree alg h = let P (SConst b) tc = scata alg h in (b, tc)
+
+specToAlg
+  :: Patchable h h'
+  => h (SConst b) :-> (SConst b :*: h' (SConst ()))
+  -> h (SConst b :*: SFix h') :-> (SConst b :*: SFix h')
+specToAlg spec level =
+  case shapeDist level of
+    P bs cs ->
+      let P b c = spec bs
+      in P b (SFix $ patch cs c)
+
+sMapAccumAlg
+  :: ShapeFunctor h
+  => (h (SConst b :*: SFix h'):-> (SConst b :*: SFix h'))
+  -> SFix h s
+  -> (b, SFix h' s)
+sMapAccumAlg alg h = let P (SConst b) h' = scata alg h in (b, h')
+
+sMapAccumSpec
+  :: Patchable h h'
+  => h (SConst b) :-> (SConst b :*: h' (SConst ()))
+  -> SFix h s
+  -> (b, SFix h' s)
+sMapAccumSpec spec = sMapAccumAlg (specToAlg spec)
